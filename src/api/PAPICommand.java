285c1eb9c52c96b578d8a73aa2b908b75da8bc66
Andrew Nisbet <anisbet@epl.ca>
2013-11-16 15:03:01 -0700

    Touched up the HTTP handling.

--- src/api/PAPICommand.java
+++ src/api/PAPICommand.java
@@ -20,8 +20,14 @@
  */
 package api;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import mecard.exception.ConfigurationException;
 
 /**
@@ -31,6 +37,8 @@ import mecard.exception.ConfigurationException;
 public class PAPICommand implements Command
 {
     private final URL url;
+    private String httpVerb;
+    private String contentType;
     
     public static class Builder
     {
@@ -40,13 +48,16 @@ public class PAPICommand implements Command
         private String loginUserId;
         private String loginStationId;
         private String verb;
+        private String contentType;
 
         /**
          * Constructor that insists that the command gets at least a status
          * object and a command.
          */
         public Builder()
-        {  }
+        { 
+            this.contentType = "json"; // default content type, also try XML.
+        }
 
         /**
          * Builds the command and returns a reference to it.
@@ -72,6 +83,12 @@ public class PAPICommand implements Command
         public Builder setLoginBranchId(String loginBranchId)
         {
             this.loginBranchId = loginBranchId;
+            return this;
+        }
+        
+        public Builder setContentType(String type)
+        {
+            this.contentType = type;
             return this;
         }
 
@@ -106,12 +123,45 @@ public class PAPICommand implements Command
                     "The PAPI URL is malformed. Please check "
                     + "your configuration for errors.");
         }
+        this.httpVerb    = papiBuilder.verb;
+        this.contentType = papiBuilder.contentType;
     }
 
     @Override
     public CommandStatus execute()
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        CommandStatus status = new CommandStatus();
+        try
+        {
+            HttpURLConnection connection = (HttpURLConnection) this.url.openConnection();
+            connection.setRequestMethod(this.httpVerb);
+            connection.setRequestProperty("Accept", "application/"+this.contentType);
+            
+            if (connection.getResponseCode() != 200) 
+            {
+                // Depending on the error we should return a more meaningful exception.
+//                throw new RuntimeException("Failed : HTTP error code : "
+//                        + connection.getResponseCode());
+                status.setStderr(String.valueOf(connection.getResponseCode()));
+            }
+            else // Non-200 code returned.
+            {
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                        (connection.getInputStream())));
+
+                String output;
+                while ((output = br.readLine()) != null) 
+                {
+                    status.setStdout(output);
+                }
+                connection.disconnect();
+            }
+        }
+        catch (IOException ex)
+        {
+            status.setStderr(String.valueOf(ex.getMessage()));
+        }
+        return status;
     }
 
 }
