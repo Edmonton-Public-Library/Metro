48371034e7a7eadb32f7b41bf6afe6eb8ca002ca
metro <anisbet@epl.ca>
2014-02-18 14:20:31 -0700

    Added tests for lost card creation.

--- test/mecard/customer/UserLostFileTest.java
+++ test/mecard/customer/UserLostFileTest.java
@@ -2,6 +2,8 @@
 package mecard.customer;
 
 import api.CommandStatus;
+import java.io.File;
+import mecard.config.CustomerFieldTypes;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -19,6 +21,7 @@ public class UserLostFileTest
     
     public UserLostFileTest()
     {
+        
     }
 
     /**
@@ -27,12 +30,20 @@ public class UserLostFileTest
     @Test
     public void testSetStatus()
     {
-        System.out.println("setStatus");
-        CommandStatus status = null;
-        UserLostFile instance = null;
-        instance.setStatus(status);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("==setStatus==");
+        String name = "Doe, John";
+        Customer instance = new Customer();
+        instance.setName(name);
+        instance.set(CustomerFieldTypes.ISLOSTCARD, "Y");
+        boolean test = instance.isLostCard();
+        assertTrue(instance.isLostCard());
+        instance.set(CustomerFieldTypes.ALTERNATE_ID, "22222012345678");
+        instance.set(CustomerFieldTypes.ID, "22222012222222");
+        UserLostFile userFile = new UserLostFile(instance);
+        userFile.recordUserDataMessage("LOST CARD detected. This message is read from message.properties normally.");
+        File f = new File("logs/22222012222222.lost");
+        assertTrue(f.exists());
+        f.delete();
     }
     
 }
