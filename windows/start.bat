0f68d8be7202a08a83598dfdd834a8b61f4c5238
metro <anisbet@epl.ca>
2014-04-28 09:06:25 -0600

    Removed Window's bat files since they are generated by running the install instead.

--- windows/start.bat
+++
@@ -1,2 +0,0 @@
-REM Executes Service (ES) Metro.
-c:\metro\windows\prunsrv.exe //ES//Metro --Jvm=auto --Startup=auto --StartMode=jvm --Classpath=c:\metro\dist\MeCard.jar --StartClass=mecard.MetroService --StartMethod=start ++StartParams=-c;c:\metro\ --StopMode=jvm --StopClass=mecard.MetroService --StopMethod=stop --LogPath=c:\metro\logs --LogLevel=Info --LogPrefix=metro --StdOutput=c:\metro\logs\stdout.txt --StdError=c:\metro\logs\stderr.txt
--- windows/uninstall.bat
+++
@@ -1,2 +0,0 @@
-REM Uninstalls metro. It may be necessary to clean your registry- we used CClean.
-c:\metro\windows\prunsrv.exe //DS//Metro
