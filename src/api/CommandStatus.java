a57c7cec762c8676856855309ed2242c29067036
Andrew Nisbet <anisbet@epl.ca>
2020-10-02 19:29:22 -0600

    Fixed bug that put newlines on stdout when SIPCommand updates CommandStatus during SIP2 calls.

--- src/api/CommandStatus.java
+++ src/api/CommandStatus.java
@@ -1,6 +1,6 @@
 /*
  * Metro allows customers from any affiliate library to join any other member library.
- *    Copyright (C) 2013  Edmonton Public Library
+ *    Copyright (C) 2020  Edmonton Public Library
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,7 +22,12 @@ package api;
 
 import mecard.ResponseTypes;
 
-
+/**
+ * Represents the results of the execution of an external system command including
+ * the exit status and any stderr / stdout results.
+ * 
+ * @author Andrew Nisbet andrew.nisbet@epl.ca andrew@dev-ils.com
+ */
 public class CommandStatus
 {
     protected ResponseTypes status;
@@ -36,23 +41,44 @@ public class CommandStatus
         this.status = ResponseTypes.INIT;
     }
     
+    /**
+     * Sets the status of the command to BUSY, to indicate that the process
+     * started.
+     */
     void setStarted()
     {   
         this.status = ResponseTypes.BUSY;
     }
 
+    /** 
+     * Sets the standard output of the command, with a system-dependent
+     * newline character appended to the last line of output.
+     * 
+     * @param line or lines of text from standard out.
+     */
     void setStdout(String line)
     {
         this.stdout.append(line);
         this.stdout.append("\n");
     }
 
+    /** 
+     * Sets the standard error of the command, with a system-dependent
+     * newline character appended to the last line of output.
+     * 
+     * @param line or lines of text from standard out.
+     */
     void setStderr(String line)
     {
         this.stderr.append(line);
         this.stderr.append("\n");
     }
 
+    /** 
+     * Appends the exit status of the command to stdout and stderr, then adds a
+     * newline character to each output stream when done.
+     * @param value exit value of the process as an integer.
+     */
     void setEnded(int value)
     {
         this.stdout.append(value);
@@ -62,13 +88,19 @@ public class CommandStatus
         this.status = ResponseTypes.COMMAND_COMPLETED;
     }
 
-    void setError(Throwable th)
+    /**
+     * Allows the caller to set an error for the command status in the form of
+     * a Java throw-able object for logging and diagnosis issues.
+     * @param e the error that was thrown during command execution.
+     */
+    void setError(Throwable e)
     {
-        this.stderr.append(th.getMessage());
+        this.stderr.append(e.getMessage());
         this.status = ResponseTypes.ERROR;
     }
     
-    /** Allows specific {@link ResponseTypes} to be recorded. {@link #setEnded(int) }
+    /** 
+     * Allows specific {@link ResponseTypes} to be recorded. {@link #setEnded(int) }
      * performs a similar task but the response code is always set to {@link ResponseTypes#COMMAND_COMPLETED}
      * This method will actually set the the status to a value that can be used
      * later for computing the success of one command before continuing onto another.
@@ -84,17 +116,39 @@ public class CommandStatus
         this.stderr.append("\n");
         this.status = value;
     }
+    
+    /**
+     * Sets the response type of the command without any formatting.
+     * @param type the command's exit status translated into a form the 
+     * MeCard server can understand.
+     */
+    void setResponse(ResponseTypes type)
+    {
+        this.status = type;
+    }
 
+    /**
+     * Standard getter for the status.
+     * @return the exit status of the command as a MeCard understandable type.
+     */
     public ResponseTypes getStatus() 
     {
         return this.status;
     }
 
+    /**
+     * Standard getter of the standard out of the completed command.
+     * @return string or strings from the command's standard out stream.
+     */
     public String getStdout() 
     {
         return this.stdout.toString();
     }
 
+    /**
+     * Standard getter of the standard error of the completed command.
+     * @return string or strings from the command's standard error stream.
+     */
     public String getStderr() 
     {
         return this.stderr.toString();
