cb349aa4b75a40b53c81d49748f78d3ad146df0e
Andrew Nisbet <anisbet@epl.ca>
2021-02-18 20:59:54 -0500

    Code clean up for loading users.

--- unix/loaduser.sh
+++ unix/loaduser.sh
@@ -2,7 +2,7 @@
 ########################################################################
 #
 # Importer for systems that use SSH for loading customers.
-#    Copyright (C) 2017  Andrew Nisbet
+#    Copyright (C) 2020  Andrew Nisbet
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -12,7 +12,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU General Public License for more detaILS_OS.
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
@@ -21,20 +21,25 @@
 #
 # Author:  Andrew Nisbet, Edmonton Public Library
 # Rev:
+#          0.4 - Add processing for Windows users.
 #          0.3 - Create tested, now to add update.
 #          0.2 - Dev.
 #
 ########################################################################
 ### Site specific values
-SSH_SERVER="sirsi@edpl-t.library.ualberta.ca"  # ILS site specific
+HOME=/home/metro_user/metro
+SSH_SERVER="sirsi@some.library.ca"  # ILS's Operating System site specific
 # This value is used to compile the customer data into a form consumable by the 'here-doc'.
-ILS="SOLARIS"                  # Type of unix the ILS is running, site specific, see below.
-LIBRARY="EPLMNA"               # Default library for customer, site specific
+ILS_OS=SOLARIS                 # Valid ILS OS are RedHat, Solaris, Windows
+LIBRARY=EPLMNA              # Default library (or branch) for customer, site specific
 # The error log can go anywhere and be named anything, but this makes sense.
-ERROR="/home/ils/metro/logs/loaduser.log"
+ERROR="$HOME/logs/loaduser.log"
 # The password file can be named anything and can be located anywhere
 # but it should only include the password and no other lines or characters.
-PASSWORD_FILE="/home/ils/metro/config/loaduser_ssh.txt"
+PASSWORD_FILE="$HOME/config/loaduser_ssh.txt"
+# Make sure SCP has permissions to write to this directory.
+# Could be set to ${SIRSI_HOME}\Unicorn\Sirsiserv
+WINDOWS_TEMP_DIR="C:\\Windows\\Temp"
 ###
 # This script is able to load customers on the ILS even if the ILS doesn't 
 # have a public SSH key for this server. You can also specify special variables
@@ -53,55 +58,90 @@ PASSWORD=$(cat "$PASSWORD_FILE" 2>>$ERROR)
 # Loads customer data from file and executes it via SSH adding variables as
 # required.
 # param:  flat file of customer data.
-# return: 0 if everything went well and 1 otherwise.
 load_customer()
 {
         if [ -z "$1" ]; then
-                echo "*** error empty argument." >>$ERROR
-                return
+                echo "*** error expected the name of a flat file as an argument but got nothing." >>$ERROR
+                echo "*** error expected the name of a flat file as an argument but got nothing." >&2
+                exit 1
         fi
 
         # Save customer ID for error log.
         local user_id=$(grep USER_ID "$1" | awk 'NF>1{print $NF}' | sed -e 's/^..//')
         echo "user_id: $user_id" >>$ERROR
+        echo "user_id: $user_id" >&2
         # API to use, default is to update if exists and create if not.
         # Add a trailing '\' to each line - except the last - in this
         # way we can echo all the flat data from one variable.
-        if [ "$ILS" == "REDHAT" ]; then
-                local customer=$(cat "$1" | sed -e '$ ! s/$/\n\\/')    # Required at Shortgrass.
-        elif [ "$ILS" == "SOLARIS" ]; then
-                local customer=$(cat "$1" | sed -e '$ ! s/$/\\/')      # Ubuntu at EPL.
+        if [ "$ILS_OS" == "REDHAT" ]; then
+                local customer=$(cat "$1" | sed -e '$ ! s/$/\n\\/')
+        elif [ "$ILS_OS" == "SOLARIS" ]; then
+                local customer=$(cat "$1" | sed -e '$ ! s/$/\\/')   # Obsolete OS; be warned!.
+        elif [ "$ILS_OS" == "WINDOWS" ]; then
+                # local customer=$(cat "$1" | sed -e '$ ! s/$/\\/') # TODO: does this need any special EOL processing?
+                echo "Windows selected. '$WINDOWS_TEMP_DIR'" >>$ERROR
+                echo "Windows selected. '$WINDOWS_TEMP_DIR'" >&2
         else
                 local customer=$(cat "$1" | sed -e '$ ! s/$/\n\\/')    # Default to modern unix.
         fi
-        # If there is no password then we assume the ILS has a public key for this machine.
+        # If there is no password then we assume the ILS_OS has a public key for this machine.
+        if [ "$ILS_OS" == "WINDOWS" ]; then
+                scp "$1" "${SSH_SERVER}:$WINDOWS_TEMP_DIR"
+                load_on_windows "$WINDOWS_TEMP_DIR\\$1"
+        else
+                if [ -z "$PASSWORD" ]; then
+                        ssh -t "$SSH_SERVER" << EOSSH 2>>$ERROR
+echo "$customer" | loadflatuser $API_SWITCHES -n -y$LIBRARY -l"$USER"
+exit
+EOSSH
+                else # We use this to authenticate automatically without typing the password.
+                        sshpass -p"$PASSWORD" ssh -t "$SSH_SERVER" << EOSSH 2>>$ERROR
+echo "$customer" | loadflatuser $API_SWITCHES -n -y$LIBRARY -l"$USER"
+exit
+EOSSH
+                fi # End of Unix load customer
+        fi # End of test for Windows customer load.
+}
+
+# Loads the customer data string version of their flat file onto a unix based ILS.
+# param:  customer flat file with fully qualified path on the Windows system.
+load_on_windows()
+{
+        local flat_file="$1"
         if [ -z "$PASSWORD" ]; then
                 ssh -t "$SSH_SERVER" << EOSSH 2>>$ERROR
-echo "$customer" | loadflatuser $API_SWITCHES -n -y$LIBRARY -l"$USER"
+type "$flat_file" | loadflatuser $API_SWITCHES -n -y$LIBRARY -l"$USER"
 exit
 EOSSH
         else # We use this to authenticate automatically without typing the password.
                 sshpass -p"$PASSWORD" ssh -t "$SSH_SERVER" << EOSSH 2>>$ERROR
-echo "$customer" | loadflatuser $API_SWITCHES -n -y$LIBRARY -l"$USER"
+type "$flat_file" | loadflatuser $API_SWITCHES -n -y$LIBRARY -l"$USER"
 exit
 EOSSH
         fi
 }
+
+# Main operations of the script start here.
 if [ -z "$2" ]; then
-        echo "*WARN expected argument 'CREATE' or 'UPDATE', but got none. Defaulting to $API_SWITCHES" >>$ERROR
+        echo "**error, expected argument 'CREATE' or 'UPDATE', but got nothing." >>$ERROR
+        echo "**error, expected argument 'CREATE' or 'UPDATE', but got nothing." >&2
+        exit 3
 else
         case "$2" in
         UPDATE)
                 echo "UPDATE selected.\n" >>$ERROR
+                echo "UPDATE selected.\n" >&2
                 API_SWITCHES="-aR -bR -mu"
                 ;;
         CREATE)
-                echo "CREATE selected.\n" >>$ERROR
+                echo "CREATE selected.\n" >$ERROR
+                echo "CREATE selected.\n" >&2
                 API_SWITCHES="-aU -bU -mc"
                 ;;
 	*)
-		echo "*WARN expected argument 'CREATE' or 'UPDATE', but got '$2'. Defaulting to $API_SWITCHES" >>$ERROR
-                API_SWITCHES="-aR -bR -mu"
+		echo "*WARN expected argument 'CREATE' or 'UPDATE', but got '$2'." >>$ERROR
+		echo "*WARN expected argument 'CREATE' or 'UPDATE', but got '$2'." >&2
+                exit 3
 		;;
         esac
 fi
@@ -109,11 +149,16 @@ if [ -s "$1" ]; then
         result=$(load_customer "$1")
         if [ -z "$result" ]; then
                 echo "*** error occurred while creating or updating customer account."  >>$ERROR
+                echo "*** error occurred while creating or updating customer account."  >&2
+                exit 1
         else
                 echo "user_key: $result successfully loaded." >>$ERROR
+                echo "user_key: $result successfully loaded." >&2
         fi
 else
         echo "*** error argument entered is either empty, not a file, or the file was not found." >>$ERROR
+        echo "*** error argument entered is either empty, not a file, or the file was not found." >&2
+        exit 2
 fi
+exit 0
 # EOF
-
