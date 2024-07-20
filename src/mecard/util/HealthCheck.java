0ec4e2161d075847dfea80271f1160e5589cf266
Andrew Nisbet <anisbet@epl.ca>
2024-07-20 00:37:01 -0400

    Information now reported as String, ready for use in Response objects.

--- src/mecard/util/HealthCheck.java
+++ src/mecard/util/HealthCheck.java
@@ -1,4 +1,23 @@
-
+/*
+ * Metro allows customers from any affiliate library to join any other member library.
+ *    Copyright (C) 2024  Edmonton Public Library
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
 package mecard.util;
 
 import java.io.File;
@@ -16,10 +35,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.concurrent.TimeUnit;
 
 /**
- *
+ * Utility class to report important environment diagnostic information.
  * @author Andrew Nisbet <andrew at dev-ils.com>
  */
 public class HealthCheck
@@ -28,60 +46,31 @@ public class HealthCheck
      * Get the server up time. Does its best to work with Unix, Mac, and Windows.
      * @return up time in hours.
      */
-    public static long getServerUptime() 
+    public static String getServerUptime() 
     {
         String os = System.getProperty("os.name").toLowerCase();
+        StringBuilder outLine = new StringBuilder();
         try 
         {
+            String line;
             if (os.contains("win")) {
+                long value = 0;
                 Process uptimeProc = Runtime.getRuntime().exec("net stats srv");
                 BufferedReader in = new BufferedReader(new InputStreamReader(uptimeProc.getInputStream()));
-                String line;
                 while ((line = in.readLine()) != null) {
-                    if (line.startsWith("Statistics since")) {
-                        String[] parts = line.split("\\s+");
-                        String[] timeParts = parts[parts.length - 1].split(":");
-                        return TimeUnit.HOURS.toSeconds(Integer.parseInt(timeParts[0])) +
-                               TimeUnit.MINUTES.toSeconds(Integer.parseInt(timeParts[1])) +
-                               Integer.parseInt(timeParts[2]);
-                    }
+                    outLine.append(line).append("\n");
                 }
             } 
             else if (os.contains("mac") || os.contains("nix") || os.contains("nux") || os.contains("aix")) 
             {
                 Process uptimeProc = Runtime.getRuntime().exec("uptime");
                 BufferedReader in = new BufferedReader(new InputStreamReader(uptimeProc.getInputStream()));
-                String line = in.readLine();
-                if (line != null) {
-                    // Pattern to match different uptime formats
-                    Pattern pattern = Pattern.compile("up\\s+(\\d+)\\s+days?|up\\s+(\\d+):?(\\d+)|(\\d+)\\s+min");
-                    Matcher matcher = pattern.matcher(line);
-
-                    long days = 0, hours = 0, minutes = 0;
-
-                    while (matcher.find()) {
-                        if (matcher.group(1) != null) {
-                            // Days
-                            days = Long.parseLong(matcher.group(1));
-                        } else if (matcher.group(2) != null) {
-                            // Hours and optionally minutes
-                            hours = Long.parseLong(matcher.group(2));
-                            if (matcher.group(3) != null) {
-                                minutes = Long.parseLong(matcher.group(3));
-                            }
-                        } else if (matcher.group(4) != null) {
-                            // Only minutes
-                            minutes = Long.parseLong(matcher.group(4));
-                        }
-                    }
-                    // hours
-                    return ((days * 24 * 60 * 60) + (hours * 60 * 60) + (minutes * 60)) / (60 * 60);
-                }
+                outLine.append(in.readLine());
             }
         } 
         catch (IOException | NumberFormatException e) 
         {  }
-        return -1; // Return -1 if unable to determine uptime
+        return outLine.length() != 0 ? outLine.toString() : "-1";
     }
 
     /**
@@ -104,7 +93,7 @@ public class HealthCheck
      */
     public static int countLogTransactions(String filePath) throws IOException 
     {
-        int customerCount = 0;
+        int transactionCount = 0;
         Pattern pattern = Pattern.compile("^\\w{3} \\w{3} \\d{2} \\d{2}:\\d{2}:\\d{2} \\w{3} \\d{4} transaction completed\\.$");
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) 
@@ -115,12 +104,12 @@ public class HealthCheck
                 Matcher matcher = pattern.matcher(line);
                 while (matcher.find()) 
                 {
-                    customerCount++;
+                    transactionCount++;
                 }
             }
         }
 
-        return customerCount;
+        return transactionCount;
     }
     
     /**
@@ -192,29 +181,39 @@ public class HealthCheck
      * <li>Last activity of the MeCard server.</li>
      * <li>Host disk space.</li>
      * @param filePath 
+     * @return System information in a String.
      */
-    public static void getServerHealth(String filePath)
+    public static String getServerHealth(String filePath)
     {
         String logFilePath = filePath + "/metro.out";
         String errFilePath = filePath + "/metro.err";
         String diskPath = "/"; // Root directory for disk space check
-
+        StringBuilder outStr = new StringBuilder();
         try 
         {
-            System.out.println("MeCard version number: " + getLastVersionNumber(logFilePath));
-            System.out.println("Server uptime: " + getServerUptime() + " hours");
-            System.out.println("Out file size: " + getLogFileSize(logFilePath) + " KB");
-            System.out.println("Err file size: " + getLogFileSize(errFilePath) + " KB");
-            System.out.println("Number of transactions: " + countLogTransactions(logFilePath));
-            
+            outStr.append(System.getProperty("os.name"))
+                    .append(" version: ")
+                    .append(System.getProperty("os.version"))
+                    .append(" arch:")
+                    .append(System.clearProperty("os.arch"))
+                    .append("\n");
+            outStr.append("Java: ").append(System.getProperty("java.version")).append("\n");
+            outStr.append("MeCard version: ").append(getLastVersionNumber(logFilePath)).append("\n");
+            outStr.append("User: ").append(System.getProperty("user.name")).append("\n");
+            outStr.append("User dir: ").append(System.getProperty("user.dir")).append("\n");
+            outStr.append("Up time: ").append(getServerUptime()).append(" hours").append("\n");
             LocalDateTime lastActivity = getLastActivityDateTime(logFilePath);
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            System.out.println("Last activity: " + lastActivity.format(formatter));
-            System.out.println("Host disk space: " + getHostDiskSpace(diskPath));
-            
+            outStr.append("Last active: ").append(lastActivity.format(formatter)).append("\n");
+            outStr.append("Host disk: ").append(getHostDiskSpace(diskPath)).append("\n");
+            outStr.append("Log size: ").append(getLogFileSize(logFilePath)).append(" KB").append("\n");
+            outStr.append("Err size: ").append(getLogFileSize(errFilePath)).append(" KB").append("\n");
+            outStr.append("Transactions: ").append(countLogTransactions(logFilePath)).append("\n");
         } catch (IOException e) {
-            System.out.println("***error (IO exception) while checking server health!");
+            outStr.append("***error (IO exception) while checking server health!")
+                    .append(" Is the log file missing?").append("\n");
         }
+        return outStr.toString();
     }
     
     /**
@@ -224,6 +223,6 @@ public class HealthCheck
     public static void main(String[] args) 
     {
         String logPath = "logs";
-        getServerHealth(logPath);
+        System.out.println(getServerHealth(logPath));
     }
 }
