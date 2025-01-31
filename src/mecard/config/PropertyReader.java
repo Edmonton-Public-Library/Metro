e79349bc3ed17658076cbb9a42c6fed1432a8a78
Andrew Nisbet <andrew.nisbet@epl.ca>
2025-01-31 15:43:25 -0500

    Added duplicate user reporting to Papi.

--- src/mecard/config/PropertyReader.java
+++ src/mecard/config/PropertyReader.java
@@ -42,7 +42,7 @@ import mecard.util.PlaceNameWGet;
  */
 public class PropertyReader
 {
-    public final static String VERSION           = "2.04.02a"; // server version
+    public final static String VERSION           = "2.04.02e"; // server version
     /** Including this tag with a value like 'user&#64;server.com', will cause 
      * commands to be run remotely through secure shell (ssh).
      * The tag is optional. Leaving it out means 
